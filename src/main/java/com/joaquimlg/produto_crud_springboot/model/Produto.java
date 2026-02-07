5bc0f30b5620d480b25176db93ade4be49bdffa3
JoaquimLG <joaquimlaureanog7@gmail.com>
2026-02-06 21:04:16 -0300

    feat: add Produto entity

--- src/main/java/com/joaquimlg/produto_crud_springboot/model/Produto.java
+++ src/main/java/com/joaquimlg/produto_crud_springboot/model/Produto.java
@@ -1,4 +1,27 @@
 package com.joaquimlg.produto_crud_springboot.model;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "produtos")
+
 public class Produto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private double preco;
+    private Integer quantidade;
+
+    public Produto(String nome, double preco, Integer quantidade) {
+        this.nome = nome;
+        this.preco = preco;
+        this.quantidade = quantidade;
+    }
 }
