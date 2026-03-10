8fc5e8ebb18aa2e2d1e3e99b7452f031916ba785
JoaquimLG <joaquimlaureanog7@gmail.com>
2026-03-10 17:09:30 -0300

    feat: add validation for name and quantity

--- src/main/java/com/joaquimlg/produto_crud_springboot/model/Produto.java
+++ src/main/java/com/joaquimlg/produto_crud_springboot/model/Produto.java
@@ -1,6 +1,7 @@
 package com.joaquimlg.produto_crud_springboot.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.PositiveOrZero;
 import lombok.Getter;
@@ -17,11 +18,16 @@ public class Produto {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotBlank(message = "O nome não pode estar em branco")
     private String nome;
 
     @NotNull(message = "O preço é obrigatório")
     @PositiveOrZero(message = "O preço não pode ser negativo")
     private Double preco;
+
+    @NotNull(message = "A quantidade é obrigatória")
+    @PositiveOrZero(message = "A quantidade não pode ser negativa")
     private Integer quantidade;
 
     public Produto(String nome, double preco, Integer quantidade) {
